38f479f997d5cbcdfafc606aedba23574cc4e431
hawk82 <ermolin82@gmail.com>
2013-09-11 05:32:59 -0700

    First-shot TD

---
+++ TD_1009/Assets/EnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.tag = "Enemy";
+		gameObject.renderer.material.color = new Color(0, 1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(-0.1f, 0.0f, 0.0f));
+	}
+}
---
+++ TD_1009/Assets/MissleScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.renderer.material.color = new Color(1, 0, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_target == null)
+		{
+			Destroy (gameObject);
+			return;
+		}
+		Vector3 dir = m_target.transform.position - transform.position;
+		if (dir.magnitude < 0.2)
+		{
+			Destroy(m_target);
+			Destroy(gameObject);
+		}
+		else
+		{
+			dir.Normalize();
+			dir *= 0.2f;
+			transform.Translate(dir);
+		}
+		
+	}
+	public GameObject m_target;
+}
