d11ee4275cb932d838ce99d7667709d0239727fd
hawk82 <ermolin82@gmail.com>
2013-09-27 21:38:39 -0700

    More robust check that target is hit

--- TD_1009/Assets/Script_Projectile/TDProjectile.cs
+++ TD_1009/Assets/Script_Projectile/TDProjectile.cs
@@ -16,22 +16,28 @@ public abstract class TDProjectile : MonoBehaviour {
 		}
 		moveToTarget();
 		Vector3 dir = m_target.transform.position - transform.position;
-		if (dir.magnitude < TDWorld.getWorld().m_configuration.hitDistance)
+		float dist = dir.magnitude;
+		// Second condition prevents round-off situation when target is almost reached but not damaged
+		if ((dist < TDWorld.getWorld().m_configuration.hitDistance) ||
+			(dist > (m_recDistance + TDWorld.getWorld().m_configuration.hitDistance)))
 		{
 			onTargetReached();
 			Destroy(gameObject);
 		}
+		m_recDistance = dist;
 	}
 
 	public virtual void setTarget(TDActor target)
 	{
 		m_target = target;
+		m_recDistance = (m_target.transform.position - transform.position).magnitude;
 	}
 
 	public abstract float speed();
 	public abstract void moveToTarget();
 	public abstract void onTargetReached();
 	
+	float m_recDistance;
 	public TDDamage m_damage;
 	protected TDActor m_target;
 }
